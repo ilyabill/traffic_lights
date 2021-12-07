ea28e87156f2077759795ea6e4ada36c334b4b63
ilyabill <bilarusilya@gmail.com>
2021-12-07 18:27:31 +0300

    design finish

--- traffic_lights/ViewController.swift
+++ traffic_lights/ViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+        redView.alpha = 0.25
+        yellowView.alpha = 0.25
+        greenView.alpha = 0.25
+        startButton.layer.cornerRadius = 20
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        redView.layer.cornerRadius = redView.frame.height/2
+        yellowView.layer.cornerRadius = yellowView.frame.height/2
+        greenView.layer.cornerRadius = greenView.frame.height/2
+        
+    }
+    
+    
+    
+    @IBAction func changeColorButton() {
+
+        
+    }
+    
 
 
 }
