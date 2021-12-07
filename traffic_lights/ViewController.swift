42d470d67b5ef86dd5965887ecd28309a0c05998
ilyabill <bilarusilya@gmail.com>
2021-12-07 17:36:39 +0300

    Initial Commit

---
+++ traffic_lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic_lights
+//
+//  Created by ILYA BILARUS on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
